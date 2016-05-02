82411cf814c546d44e4d718a38d72a749e0e7dc7
Robin Hossain <robinhossainbd@gmail.com>
2016-05-02 10:33:24 +0600

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -3,3 +3,6 @@
 
 // Query For Finding Duplicate Data
 SELECT id, COUNT(id) c FROM `crcalls` GROUP BY id HAVING c>1
+
+// Query for import sql file with command/terminal
+mysql -u username -p database_name -v < C:\xampp\htdocs\file.sql
