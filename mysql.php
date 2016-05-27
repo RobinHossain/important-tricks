5d643b3e4bc9190113f9785aaaa88e5f1f8902fd
Robin Hossain <robinhossainbd@gmail.com>
2016-05-27 11:46:24 +0600

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -6,3 +6,6 @@ SELECT id, COUNT(id) c FROM `crcalls` GROUP BY id HAVING c>1
 
 // Query for import sql file with command/terminal
 mysql -u username -p database_name -v < C:\xampp\htdocs\file.sql
+
+// Query for Export sql file with command/terminal
+mysqldump -u username -p database_name > C:\xampp\htdocs\file.sql
